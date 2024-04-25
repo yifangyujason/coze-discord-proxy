f95631aea7de61c8d091997f510d00c7b465e64c
yifangyujason <996335280@qq.com>
2024-04-25 11:23:32 +0800

    feat:添加错误提示

--- common/constants.go
+++ common/constants.go
@@ -45,6 +45,7 @@ var CozeErrorMsg = "Something wrong occurs, please retry. If the error persists,
 
 var CozeErrorMessages = []string{"You have exceeded the daily limit for sending messages to the bot. Please try again later.",
 	"Something wrong occurs, please retry. If the error persists, please contact the support team.",
+	"Some error occurred. Please try again or contact the support team in our communities.",
 	"There are too many users now. Please try again a bit later.",
 	"I'm sorry, but I can't assist with that."}
 
